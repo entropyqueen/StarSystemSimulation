2481891f1fe077b500c42e787940e2f389a038a9
entropyQueen <emy.rg@protonmail.com>
2022-12-29 11:25:59 +0100

    Saving progress -- Add rotation with mouse

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ import config
 if __name__ == '__main__':
 
     viewer = Visualizer()
+    # Load Universe passing viewer'
+    #       Universe class shall register it's update() method to viewer
+    # Load planets passing viewer
+    #   planets will have a RenderObject or similar that will store:
+    #       obj_orbit = universe.main_star.attachNewNode(f'orbit_{self.name}')
+    #       obj = loader.loadModel('./models/sphere')
+    #       obj_tex = loader.loadTexture('./textures/') ?
+    #       obj.setTexture(obj_tex, 1)
+    #       obj.reparentTo(universe.sun.obj_orbit)
+    #       obj.setPos(coordinates)
+    #       obj.setScale(factor * base_scale)
+    # or StarStystemBody will have a render_to object ? // le render to permet
+    # de changer de moter d'affichage, l'idée étant d'avoir un truc générique
+    # en terme de physique, mais pouvoir choisir si je le veux dans matplotlib
+    # ou dans panda3d, ou autre, whatever ce qu'on implem.
     viewer.run()
 
     plt.style.use('dark_background')
