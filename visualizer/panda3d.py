1a16056b410dd3623eab029039d5791e81644612
entropyQueen <emy.rg@protonmail.com>
2022-12-29 18:25:29 +0100

    Rewrite controls to have repeatable and one time press + add text overlay with easy use + add focus object

--- visualizer/panda3d.py
+++ visualizer/panda3d.py
@@ -1,6 +1,7 @@
 import sys
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties, Vec3
+from direct.gui.OnscreenText import OnscreenText, TextNode
 
 import config
 from utils import hex_to_rgb_norm
@@ -11,7 +12,7 @@ from visualizer.panda3d_object_display import ObjectDisplay
 
 class PandaVisualizer(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
+        super().__init__()
 
         # Window properties
         wp = WindowProperties()
@@ -20,52 +21,61 @@ class PandaVisualizer(ShowBase):
         wp.setSize(config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT)
         self.win.requestProperties(wp)
 
-        self.keymap = {
-            'FWD': False,
-            'BACKWD': False,
-            'UP': False,
-            'LEFT': False,
-            'DOWN': False,
-            'RIGHT': False,
-            'ROLL_R': False,
-            'ROLL_L': False,
-            'ZOOM_IN': False,
-            'ZOOM_OUT': False,
-            'MVT_SPEED+': False,
-            'MVT_SPEED-': False,
-            'TARGET_PREV': False,
-            'TARGET_NEXT': False,
-            'QUIT': False,
-        }
+        self.zoom_factor = config.DEFAULT_ZOOM
+        self.init_default_display()
+
+        # Initialize display settings
+        self.info_text = {}
+        self.info_text_ids = {}
+        self.objects_to_display = []
+        self.selected_object_iter = 0
+
+        # Controls initialization
+        self.keymap_rep = {}
+        for k in config.KEYMAP_REP.keys():
+            self.keymap_rep[k] = False
+        self.focus_selected = False
         self.init_controls()
         self.cam_speed = config.CAM_SPEED
         self.cam_rotation_speed = config.CAM_ROTATION_SPEED
-        self.keyboard_task = self.taskMgr.add(self.keyboard_control, 'keyboard_control')
         self.last_mouse_x, self.last_mouse_y = 0, 0
-        self.mouse_task = self.taskMgr.add(self.mouse_control, 'mouse_control')
-
-        self.init_default_display()
-        # self.foobar()
 
         # Initialize Universe, and populate with Sol
         self.universe = Universe()
         sol = create_Sol_system(self.universe)
-        self.objects_to_display = []
         for obj in sol:
             self.objects_to_display.append(ObjectDisplay(obj))
-        self.selected_object_iter = 0
         self.selected_object = self.objects_to_display[self.selected_object_iter]
+        self.info('sim_date', f'Simulation date: {self.universe.str_date}')
+
+        # Launch tasks
         self.update_task = self.taskMgr.add(self.update, 'update')
+        self.mouse_task = self.taskMgr.add(self.mouse_control, 'mouse_control')
+        self.keyboard_task = self.taskMgr.add(self.keyboard_control, 'keyboard_control')
+        self.focus_camera_on(self.selected_object)
 
     def init_controls(self):
         self.disableMouse()
-        for k, v in config.KEYMAP.items():
+        for k, v in config.KEYMAP_REP.items():
             if v not in ['mouse_x', 'mouse_y']:
                 self.accept(v, self.update_keymap, [k, True])
                 self.accept(f'{v}-up', self.update_keymap, [k, False])
+        for k, v in config.KEYMAP_ONCE.items():
+            if k == 'ZOOM_IN':
+                self.accept(v, self.increase_zoom)
+            if k == 'ZOOM_OUT':
+                self.accept(v, self.decrease_zoom)
+            if k == 'TARGET_PREV':
+                self.accept(v, self.focus_camera_on_prev)
+            if k == 'TARGET_NEXT':
+                self.accept(v, self.focus_camera_on_next)
+            if k == 'LOCK_FOCUS':
+                self.accept(v, self.toggle_focus)
+            if k == 'QUIT':
+                self.accept(v, sys.exit)
 
     def update_keymap(self, control_name, control_state):
-        self.keymap[control_name] = control_state
+        self.keymap_rep[control_name] = control_state
 
     def init_default_display(self):
         self.setBackgroundColor(*hex_to_rgb_norm('#000000'))
@@ -77,49 +87,45 @@ class PandaVisualizer(ShowBase):
         s = self.cam_speed
         r = self.cam_rotation_speed
 
-        if self.keymap['FWD']:
+        if self.keymap_rep['FWD']:
             self.cam.setPos(self.cam, Vec3(0, s * dt, 0))
-        if self.keymap['BACKWD']:
+        if self.keymap_rep['BACKWD']:
             self.cam.setPos(self.cam, Vec3(0, -s * dt, 0))
-        if self.keymap['RIGHT']:
+        if self.keymap_rep['RIGHT']:
             self.cam.setPos(self.cam, Vec3(s * dt, 0, 0))
-        if self.keymap['LEFT']:
+        if self.keymap_rep['LEFT']:
             self.cam.setPos(self.cam, Vec3(-s * dt, 0, 0))
-        if self.keymap['UP']:
+        if self.keymap_rep['UP']:
             self.cam.setPos(self.cam, Vec3(0, 0, s * dt))
-        if self.keymap['DOWN']:
+        if self.keymap_rep['DOWN']:
             self.cam.setPos(self.cam, Vec3(0, 0, -s * dt))
-        if self.keymap['ROLL_R']:
+        if self.keymap_rep['ROLL_R']:
             self.cam.setHpr(self.cam, Vec3(0, 0, r * dt))
-        if self.keymap['ROLL_L']:
+        if self.keymap_rep['ROLL_L']:
             self.cam.setHpr(self.cam, Vec3(0, 0, -r * dt))
-        if self.keymap['MVT_SPEED+']:
+        if self.keymap_rep['MVT_SPEED+']:
             self.cam_speed += config.CAM_SPEED_STEP
-            self.cam_rotation_speed += config.CAM_ROTATION_SPEED_STEP
+            self.cam_rotation_speed *= config.CAM_ROTATION_SPEED_STEP
             if self.cam_speed > config.MAX_CAM_SPEED:
                 self.cam_speed = config.MAX_CAM_SPEED
             if self.cam_speed > config.MAX_CAM_ROTATION_SPEED:
                 self.cam_speed = config.MAX_CAM_ROTATION_SPEED
-        if self.keymap['MVT_SPEED-']:
-            self.cam_speed -= config.CAM_SPEED_STEP
+        if self.keymap_rep['MVT_SPEED-']:
+            self.cam_speed //= config.CAM_SPEED_STEP
             self.cam_rotation_speed -= config.CAM_ROTATION_SPEED_STEP
             if self.cam_speed < config.MIN_CAM_SPEED:
                 self.cam_speed = config.MIN_CAM_SPEED
             if self.cam_speed < config.MIN_CAM_ROTATION_SPEED:
                 self.cam_speed = config.MIN_CAM_ROTATION_SPEED
-        if self.keymap['ZOOM_IN']:
-            # Zooming is FWD ?
-            # Or applying a coefficient to distances / changing u size?
-            pass
-        if self.keymap['ZOOM_OUT']:
-            pass
-        if self.keymap['TARGET_PREV']:
-            self.cam.lookAt(*self.select_object('prev').pos)
-        if self.keymap['TARGET_NEXT']:
-            self.cam.lookAt(*self.select_object('next').pos)
-        if self.keymap['QUIT']:
-            sys.exit(0)
         return task.cont
+
+    def increase_zoom(self):
+        self.zoom_factor *= config.ZOOM_FACTOR_STEP
+
+    def decrease_zoom(self):
+        self.zoom_factor //= config.ZOOM_FACTOR_STEP
+        if self.zoom_factor < 1:
+            self.zoom_factor = 1
 
     def mouse_control(self, task):
         x, y, dx, dy = 0, 0, 0, 0
@@ -138,6 +144,7 @@ class PandaVisualizer(ShowBase):
     def update(self, task):
         dt = globalClock.getDt()
 
+        self.info('sim_date', f'Simulation date: {self.universe.str_date}')
         if config.VERBOSE:
             print('===============================================================')
             print(str(self.universe))
@@ -146,24 +153,81 @@ class PandaVisualizer(ShowBase):
             with open(config.CSV_OUTPUT, 'w') as f:
                 f.write(self.universe.to_csv())
         self.universe.update()
+        if self.focus_selected:
+            self.focus_camera_on(self.selected_object)
         for obj in self.objects_to_display:
+            obj.zoom_factor = self.zoom_factor
             obj.update()
-
         return task.cont
 
-    def select_object(self, direction):
-        if direction == 'next':
-            self.selected_object_iter += 1
-        else:
-            self.selected_object_iter -= 1
+    def select_object_prev(self):
+        self.selected_object_iter -= 1
         if self.selected_object_iter < 0:
             self.selected_object_iter = len(self.objects_to_display) - 1
-        elif self.selected_object_iter >= len(self.objects_to_display):
+        self.selected_object = self.objects_to_display[self.selected_object_iter]
+        return self.selected_object
+
+    def select_object_next(self):
+        self.selected_object_iter += 1
+        if self.selected_object_iter >= len(self.objects_to_display):
             self.selected_object_iter = 0
         self.selected_object = self.objects_to_display[self.selected_object_iter]
         return self.selected_object
+
+    def focus_camera_on(self, obj):
+        # TODO: Adjust zoom object to screen size
+        self.info('focus_camera', f'Focusing on {obj.obj.name}')
+        x, y, z = obj.pos
+        y -= 2 * obj.radius
+        self.cam.setPos(x, y, z)
+        self.cam.lookAt(*obj.pos)
+
+    def focus_camera_on_prev(self):
+        self.focus_camera_on(self.select_object_prev())
+
+    def focus_camera_on_next(self):
+        self.focus_camera_on(self.select_object_next())
+
+    def toggle_focus(self):
+        self.focus_selected = not self.focus_selected
 
     def foobar(self):
         axis = self.loader.loadModel('models/zup-axis')
         axis.reparentTo(self.render)
 
+    def add_info_text(self, key, text):
+
+        idx = 0
+        if len(self.info_text_ids) == 0:
+            idx = 0
+        else:
+            for idx, k in enumerate(sorted(self.info_text_ids)):
+                if idx != k:
+                    break
+                else:
+                    idx += 1
+        self.info_text[key] = idx
+        self.info_text_ids[idx] = OnscreenText(
+            text=text, pos=(0.06, -.06 * (idx + 0.5)), fg=(1, 1, 1, 1),
+            parent=self.a2dTopLeft, align=TextNode.ALeft, scale=.04
+        )
+
+    def info(self, key, text):
+        try:
+            if key not in self.info_text:
+                self.add_info_text(key, text)
+            else:
+                idx = self.info_text[key]
+                if isinstance(self.info_text_ids[idx], OnscreenText):
+                    self.info_text_ids[idx].setText(text)
+        except KeyError:
+            # nothing to update
+            pass
+
+    def del_info_text(self, key):
+        try:
+            if isinstance(self.info_text_ids[self.info_text[key]], OnscreenText):
+                self.info_text_ids[self.info_text[key]].destroy()
+        except KeyError:
+            # nothing to delete
+            pass
